307235b479da9d382a2b4a2c1e6317c088d81262
pipelonso <andresfelipeibanezcuta2@gmail.com>
2023-07-15 18:24:53 -0500

    Mejorado sistema antierrores de MailManager para envio de correos de recuperación

--- Pynterfase/Logica/MailManager.cs
+++ Pynterfase/Logica/MailManager.cs
@@ -14,28 +14,73 @@ namespace Pynterfase.Logica
 
         public void mtdsendMail(string nombre, string correo , string asunto, string cuerpo) {
 
+            int intentosMaximos = 3;
+            int intentos = 0;
+            bool enviado = false;
 
-            var mailmessage = new MimeMessage();
-
-            mailmessage.From.Add(new MailboxAddress("Pynterfase", "afibanez7@misena.edu.co"));
-            mailmessage.To.Add(new MailboxAddress(nombre, correo));
-            mailmessage.Subject = asunto;
-            mailmessage.Body = new TextPart()
+            while (!enviado && intentos < intentosMaximos)
             {
-                //Text = "HOLA! Este es tu codigo de verificación " + objVerificationE.codigo + ""
-                Text = cuerpo
-            };
+                try
+                {
+                    var mailmessage = new MimeMessage();
 
-            using (var smtpClient = new SmtpClient())
-            {
+                    mailmessage.From.Add(new MailboxAddress("Pynterfase", "afibanez7@misena.edu.co"));
+                    mailmessage.To.Add(new MailboxAddress(nombre, correo));
+                    mailmessage.Subject = asunto;
+                    mailmessage.Body = new TextPart()
+                    {
+                        Text = cuerpo
+                    };
 
-                smtpClient.Connect("smtp.gmail.com", 465, true);
-                smtpClient.Authenticate("afibanez7@misena.edu.co", "oscar y erizo3");
-                smtpClient.Send(mailmessage);
-                smtpClient.Disconnect(true);
+                    using (var smtpClient = new SmtpClient())
+                    {
+                        smtpClient.Connect("smtp.gmail.com", 465, true);
+                        smtpClient.Authenticate("afibanez7@misena.edu.co", "oscar y erizo3");
+                        smtpClient.Send(mailmessage);
+                        smtpClient.Disconnect(true);
 
+                        enviado = true;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    
 
+                    intentos++;
+                }
             }
+
+            if (!enviado)
+            {
+                //por si no se envia
+            }
+
+
+
+
+
+
+            //var mailmessage = new MimeMessage();
+
+            //mailmessage.From.Add(new MailboxAddress("Pynterfase", "afibanez7@misena.edu.co"));
+            //mailmessage.To.Add(new MailboxAddress(nombre, correo));
+            //mailmessage.Subject = asunto;
+            //mailmessage.Body = new TextPart()
+            //{
+            //    //Text = "HOLA! Este es tu codigo de verificación " + objVerificationE.codigo + ""
+            //    Text = cuerpo
+            //};
+
+            //using (var smtpClient = new SmtpClient())
+            //{
+
+            //    smtpClient.Connect("smtp.gmail.com", 465, true);
+            //    smtpClient.Authenticate("afibanez7@misena.edu.co", "oscar y erizo3");
+            //    smtpClient.Send(mailmessage);
+            //    smtpClient.Disconnect(true);
+
+
+            //}
 
 
 
