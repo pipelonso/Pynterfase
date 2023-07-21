0641b6606f5af34642bf26e3a983d2085b37c2af
pipelonso <andresfelipeibanezcuta2@gmail.com>
2023-07-20 19:32:34 -0500

    Clean (MailManager.cs) !

--- Pynterfase/Logica/MailManager.cs
+++ Pynterfase/Logica/MailManager.cs
@@ -55,35 +55,6 @@ namespace Pynterfase.Logica
                 //por si no se envia
             }
 
-
-
-
-
-
-            //var mailmessage = new MimeMessage();
-
-            //mailmessage.From.Add(new MailboxAddress("Pynterfase", "afibanez7@misena.edu.co"));
-            //mailmessage.To.Add(new MailboxAddress(nombre, correo));
-            //mailmessage.Subject = asunto;
-            //mailmessage.Body = new TextPart()
-            //{
-            //    //Text = "HOLA! Este es tu codigo de verificación " + objVerificationE.codigo + ""
-            //    Text = cuerpo
-            //};
-
-            //using (var smtpClient = new SmtpClient())
-            //{
-
-            //    smtpClient.Connect("smtp.gmail.com", 465, true);
-            //    smtpClient.Authenticate("afibanez7@misena.edu.co", "oscar y erizo3");
-            //    smtpClient.Send(mailmessage);
-            //    smtpClient.Disconnect(true);
-
-
-            //}
-
-
-
         }
 
 
